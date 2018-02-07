ada24ed668b071bdb145029bf1168876062919d9
chandlerpetersen <chandlerpetersen@outlook.com>
2018-02-06 18:27:45 -0800

    Added StudyGroup Object to populate View UI.

--- src/StudyUp/Controllers/StudyGroupController.cs
+++ src/StudyUp/Controllers/StudyGroupController.cs
@@ -9,7 +9,15 @@ namespace StudyUp.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            var group = new StudyGroup(){
+                GroupTitle = "Software Engineering I Study Group",
+                Location = "KEC 1007",
+                StartTime = new DateTime(2018, 3, 4, 12, 0, 0),
+                Duration = new TimeSpan(50000),
+                Capacity = 5,
+                Objectives = "To create the StudyUp app and review for Quiz 3!"
+            };
+            return View(group);
         }
 
         public IActionResult Join()
@@ -25,6 +33,7 @@ namespace StudyUp.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            
         }
     }
 }
