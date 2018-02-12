381272e2672820af8f1f5219bbab583880dbcd6a
Yukon Vinecki <yukonvinecki@gmail.com>
2018-02-11 18:07:43 -0800

    Add helper info the login page

--- src/StudyUp/Models/CanvasUser.cs
+++ src/StudyUp/Models/CanvasUser.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel;
+
 namespace StudyUp.Models
 {
     public class CanvasUser
     {
+        [DisplayName("Access Token")]
         public string Token { get; set; }
 
         public CanvasUser() {}
