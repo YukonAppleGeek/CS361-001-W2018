b2e786ee0e2b1c00972127fbeded22d19a188a84
Chandler Petersen <chandlerpetersen@outlook.com>
2018-02-19 18:11:39 -0800

    View study group (#12)
    * Added StudyGroup Object to populate View UI.
    
    * Added new class to link View UI with eventual populated information.
    
    * Basic HTML code for View UI. Connects to Model now.
    
    * Fixed DateTime object. Now more correct format.
    
    Still need to fix a few things.
    
    * View UI finished.
    
    Bolded text labels. Moved buttons. Added rows and columns.

---
+++ src/StudyUp/Models/StudyGroup.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace StudyUp.Models
+{
+    public class StudyGroup
+    {
+        public string GroupTitle { get; set; }
+
+        public string Location { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public TimeSpan Duration { get; set; }
+
+        public int Capacity { get; set; }
+
+        public string Objectives { get; set; }
+    }
+}
